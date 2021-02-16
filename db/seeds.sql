6ee1850d36e2dbc84f8f2ba7472d5131c920c00a
Captain <71038018+karimi65@users.noreply.github.com>
2021-02-15 22:20:50 -0600

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("burger 1");
+INSERT INTO burgers (burger_name, devoured) VALUES ("burger 3", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("burger 4", true);
+INSERT INTO burgers (burger_name) VALUES ("burger 2");
+INSERT INTO burgers (burger_name, devoured) VALUES ("burger 5", true);
